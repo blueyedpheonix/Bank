179094d81646d49095cf6e14f6710dda82615775
blueyedpheonix <57146397+blueyedpheonix@users.noreply.github.com>
2019-12-17 15:57:24 +0200

    Add files via upload

--- Bank/src/systems/Client.java
+++ Bank/src/systems/Client.java
@@ -7,14 +7,19 @@ public  class Client {
     private String name;
     private double balance;
     private Account[] accounts = new Account[10];
+    private int Acounter = 0;
     private int commissionRate = 0;
     private int interestRate = 0;
+
+    //contructor
 
     public Client(String name, int balance) {
         this.ID = IdGenerator.getClientID();
         this.name = name;
         this.balance = balance;
     }
+
+    // geters and seteres
 
     public int getID() {
         return ID;
@@ -39,38 +44,38 @@ public  class Client {
     public void setBalance(int balance) {
         this.balance = balance;
     }
+
+    //functonality
+
     public void addAccount(Account account){
-        int counter = 0;
-        while(accounts[counter] != null){
-            counter++;
+        if(Acounter >= accounts.length){
+            System.out.println("There is no more room for accounts");
+        }else{
+        accounts[Acounter] = account;
+        Acounter++;
         }
-        if(counter == accounts.length)
-            System.out.println("no more rooms for accounts");
-        else
-            accounts[counter] = account;
     }
     public Account getAccount(int i){
         return accounts[i];
     }
     public void removeAccount(int ID){
-        int counter = 0;
-        int accountID = accounts[counter].getID();
-        while(accountID != ID && counter < accounts.length){
-            if(accounts[counter] != null) {
-                accountID = accounts[counter].getID();
+        int position = -1;
+        for(int i = 0; i < Acounter; i++){
+            if(accounts[i].getID() == ID) {
+                position = i;
             }
-            counter++;
         }
-        if(accountID != ID)
-            System.out.println("no account with this ID");
-        else{
+      if(position != -1){
+          this.balance += accounts[position].getBalance();
+          accounts[position] = null;
+          for(int i = position; i < Acounter; i++){
+              accounts[i] = accounts[i+1];
+          }
+          Acounter--;
+      }else{
+          System.out.println("there is no account under this ID");
+      }
 
-            balance += accounts[counter].getBalance();
-            accounts[counter] = null;
-            for(int i = counter; i < accounts.length -1; i++){
-                accounts[i] = accounts[i+1];
-            }
-        }
     }
     public double getFortune (){
         double sum = balance;
